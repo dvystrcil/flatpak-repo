73e42ed6dc870cddf105012ab38ed698d6c33d15
Daniel Vystrcil <dvystrcil@gmail.com>
2026-05-07 09:58:07 -0700

    fix: hardcoded absolute log paths in log4j2.xml

--- mekhq-wrapper.sh
+++ mekhq-wrapper.sh
@@ -5,18 +5,6 @@
 export JAVA_HOME=/app/jre
 export PATH="${JAVA_HOME}/bin:${PATH}"
 
-# Create writable logs directory in user's XDG data home
-DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-MEKHQ_DATA="$DATA_HOME/mekhq"
-mkdir -p "$MEKHQ_DATA/logs"
-
-# Change to MekHQ directory first so absolute paths work
-cd /app/mekhq
-
-# Create symlink from /app/mekhq/logs to user's writable logs directory
-rm -rf logs
-ln -sf "$MEKHQ_DATA/logs" logs
-
-# Note: logs is now a symlink to $MEKHQ_DATA/logs
+# Note: log paths are hardcoded in log4j2.xml to /home/dan/.local/share/mekhq/logs
 
 exec /app/mekhq/bin/MekHQ "$@"
